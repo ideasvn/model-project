4da8943923e7c6b414ea75240be2c34081cf6906
hoangdv <hoangdv@qsoft.com.vn>
2016-06-24 15:29:00 +0700

    #Fix layout

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -5,40 +5,26 @@
         'ngAnimate',
         'ngStorage',
         'ui.bootstrap.datetimepicker',
-        'ui.dateTimeInput', 
+        'ui.dateTimeInput',
         'ngFileUpload',
-//        'angular-nicescroll',
-        'ngRoute',
-        'ui.router',
+        // 'angular-nicescroll',
         'QSoft.routers',
+        'QSoft.run',
         'QSoft.constants',
         'QSoft.configs',
         'QSoft.directives',
         'QSoft.filters',
         'QSoft.services',
         'QSoft.modules'
-    ]).run(['$rootScope', '$state', '$localStorage', function ($rootScope, $state, $localStorage) {
-            $rootScope.$on('$stateChangeStart', function (event, toState) {
-                var isStateLoginLogout = (toState.name === 'login' || toState.name === 'logout');
-                var tokenExpiresTime = moment().isAfter($localStorage.tokenExpiresTime);
-                var token = $localStorage.token;
-                var isToken = (_.isUndefined(tokenExpiresTime) || tokenExpiresTime || _.isUndefined(token));
-                if (isToken && !isStateLoginLogout) {
-                    event.preventDefault();
-                    //$state.go('login', {}, {reload: true});
-                }
-            });
-
-            $rootScope.$on('$stateChangeSuccess', function (event, toState, fromState) {
-//                $rootScope.metaHeadingTitle = toState.meta.title;
-            });
-        }]);
-    angular.module('QSoft.routers', []);
+    ]);
+    angular.module('QSoft.routers', ['ui.router']);
     angular.module('QSoft.configs', ['ngMeta', 'ngFlash', 'xtForm']);
     angular.module('QSoft.constants', []);
     angular.module('QSoft.directives', []);
     angular.module('QSoft.filters', []);
     angular.module('QSoft.services', ['restangular', 'ngStorage']);
     angular.module('QSoft.modules', []);
+    angular.module('QSoft.run', []);
+
 
 })();
--- src/controllers/routers.js
+++ src/controllers/routers.js
@@ -1,33 +1,45 @@
-angular.module('QSoft.routers')
+(function () {
+    "use strict";
+    angular.module('QSoft.routers')
         .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
-                $urlRouterProvider.otherwise("/404");
-                $locationProvider.html5Mode(true);
-                $stateProvider
-                        .state('404', {
-                            url: '/404',
-                            meta: {
-                                title: 'Oops! Page Not Found',
-                                description: ''
-                            },
-                            templateUrl: '/views/404/404.html'
-                        })
-                        .state('app', {
-                            abstract: true
-                        })
-                        .state('app.home', {
-                            url: '/',
+            $urlRouterProvider.otherwise("/404");
+            $locationProvider.html5Mode(true);
+            $stateProvider
+                .state('404', {
+                    url: '/404',
+                    meta: {
+                        title: 'Oops! Page Not Found',
+                        description: ''
+                    },
+                    templateUrl: '/views/404/404.html'
+                })
+                .state('app', {
+                    abstract: true,
+                    views: {
+                        'header': {
+                            templateUrl: 'views/common/header.html'
+                        },
+                        'footer': {
+                            templateUrl: 'views/common/footer.html'
+                        }
+                    }
+                })
+                .state('app.home', {
+                    url: '/',
 //                            parent: 'app',
-                            meta: {
-                                title: 'Homepage',
-                                description: ''
-                            },
-                            views: {
-                                'main@': {
-                                    templateUrl: '/views/home/index.html',
-//                            controller: ['qsPreLoader', function (qsPreLoader) {
-//                                    qsPreLoader.status(false);
-//                                }]
-                                }
-                            }
-                        });
-            }]);
+                    meta: {
+                        title: 'Homepage',
+                        description: ''
+                    },
+                    views: {
+                        'main@': {
+                            templateUrl: 'views/home/index.html',
+                            // controller: ['qsPreLoader', function (qsPreLoader) {
+                            //     qsPreLoader.status(false);
+                            // }]
+                        }
+                    }
+                });
+        }]);
+
+})();
