2d02faf170b21fa066a6eeb5e22ca060965dc0f6
datnl <datnldatnl@qsoft.com.vn>
2016-07-12 18:04:48 +0700

    pullc ode

--- src/controllers/routers.js
+++ src/controllers/routers.js
@@ -2,43 +2,54 @@
     "use strict";
     angular.module('QSoft.routers')
         .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
-            $urlRouterProvider.otherwise("/404");
-            $locationProvider.html5Mode(true);
-            $stateProvider
-                .state('404', {
-                    url: '/404',
-                    meta: {
-                        title: 'Oops! Page Not Found',
-                        description: ''
-                    },
-                    templateUrl: '/views/404/404.html'
-                })
-                .state('app', {
-                    abstract: true,
-                    views: {
-                        'header': {
-                            templateUrl: 'views/layouts/header.html'
+                $urlRouterProvider.otherwise("/404");
+                $locationProvider.html5Mode(true);
+                $stateProvider
+                    .state('404', {
+                        url: '/404',
+                        meta: {
+                            title: 'Oops! Page Not Found',
+                            description: ''
                         },
-                        'footer': {
-                            templateUrl: 'views/layouts/footer.html'
+                        templateUrl: '/views/404/404.html'
+                    })
+                    .state('app', {
+                        abstract: true,
+                        views: {
+                            'header': {
+                                templateUrl: 'views/layouts/header.html'
+                            },
+                            'footer': {
+                                templateUrl: 'views/layouts/footer.html'
+                            }
                         }
-                    }
-                })
-                .state('app.detailModel', {
-                    url: '/detail/:id',
-                    meta: {
-                        title: 'Chi Tiết Người Mẫu',
-                        description: ''
-                    },
-                    views: {
-                        'main@': {
-                            templateUrl: 'views/detail/index.html',
-                            controller: 'DetailModelCtr',
-                            controllerAs: 'DetailModelCtrAs'
+                    })
+                    .state('app.detailModel', {
+                        url: '/detail/:id',
+                        meta: {
+                            title: 'Chi Tiết Người Mẫu',
+                            description: ''
+                        },
+                        views: {
+                            'main@': {
+                                templateUrl: 'views/detail/index.html',
+                                controller: 'DetailModelCtr',
+                                controllerAs: 'DetailModelCtrAs',
+                                resolve: {
+                                    listBooking: ['$q', 'Restangular', '$stateParams', function ($q, Restangular, $stateParams) {
+
+                                            var deferred = $q.defer();
+                                            var defaultParams = {rwrel: 1, rwobj: 1};
+                                            Restangular.all('booking-list').post({idModel: $stateParams.id}).then(function (res) {
+                                                deferred.resolve(res.data);
+                                            });
+                                            return deferred.promise;
+                                        }],
+                                },
+                            }
                         }
 
-                    }
-                });
-        }]);
+                    });
+            }]);
 
 })();
--- src/modules/detail/controllers/detail.js
+++ src/modules/detail/controllers/detail.js
@@ -1,5 +1,7 @@
-angular.module('QSoft.modules').controller('DetailModelCtr', ['$scope', '$log', '$stateParams', '$compile', function ($scope, $log, $stateParams, $compile) {
+angular.module('QSoft.modules').controller('DetailModelCtr', ['$scope', '$log', '$stateParams', '$compile', 'Restangular', '$stateParams', 'listBooking', function ($scope, $log, $stateParams, $compile, Restangular, $stateParams, listBooking) {
         $scope.checkBooking = false;
+        $scope.ID = $stateParams.id;
+        $scope.events = [];
         $scope.detailModel = {
             images: [{id: 1, value: 'img/model/1.jpg'}, {id: 2, value: 'img/model/1.jpg'}, {id: 3, value: 'img/model/1.jpg'}],
             roundBrest: 90,
@@ -15,16 +17,11 @@ angular.module('QSoft.modules').controller('DetailModelCtr', ['$scope', '$log', 
         var d = date.getDate();
         var m = date.getMonth();
         var y = date.getFullYear();
-
+        console.log(listBooking);
+        angular.forEach(listBooking, function (val, key) {
+            $scope.events.push({title: val.title, start: new Date(val.startTime), end: new Date(val.endTime)});
+        })
         /* event source that contains custom events on the scope */
-        $scope.events = [
-            {title: 'All Day Event', start: new Date(y, m, 1)},
-            {title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2)},
-            {id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false},
-            {id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false},
-            {title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false},
-            {title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/'}
-        ];
 
         /* Render Tooltip */
         $scope.eventRender = function (event, element, view) {
@@ -44,14 +41,10 @@ angular.module('QSoft.modules').controller('DetailModelCtr', ['$scope', '$log', 
                     right: 'today prev,next'
                 },
                 eventClick: $scope.alertOnEventClick,
-                eventDrop: $scope.alertOnDrop,
-                eventResize: $scope.alertOnResize,
-                eventRender: $scope.eventRender,
             }
         };
 
 
         /* event sources array*/
         $scope.eventSources = [$scope.events];
-        console.log($scope.eventSources);
     }]);
